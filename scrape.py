7275eaa26101c2780b96ab2fb160a27bb3ed8cea
William Wang <bn.william.w@gmail.com>
2023-08-12 13:14:44 -0400

    mode

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
  
  
-url = 'https://base.org/'
+url = 'https://docs.mode.network/'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
  
