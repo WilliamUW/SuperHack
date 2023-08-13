6be34b3f0f648180dd50cc5261780ca07e27c704
William Wang <bn.william.w@gmail.com>
2023-08-13 10:01:10 -0400

    smart contract

--- scripts/opstack.js
+++ scripts/opstack.js
@@ -28,11 +28,11 @@ l1Contracts = {
     CanonicalTransactionChain: zeroAddr,
     BondManager: zeroAddr,
     // These contracts have the addresses you found out earlier.
-    AddressManager: "0x....",   // Lib_AddressManager.json
-    L1CrossDomainMessenger: "0x....",   // Proxy__OVM_L1CrossDomainMessenger.json  
-    L1StandardBridge: "0x....",   // Proxy__OVM_L1StandardBridge.json
-    OptimismPortal: "0x....",   // OptimismPortalProxy.json
-    L2OutputOracle: "0x....",   // L2OutputOracleProxy.json
+    AddressManager: "0xa53ac6C172cB04f2c9Ea8356fAF89eA405430e14",   // Lib_AddressManager.json
+    L1CrossDomainMessenger: "0xfB843f8c4992EfDb6b42349C35f025ca55742D33",   // Proxy__OVM_L1CrossDomainMessenger.json  
+    L1StandardBridge: "0x7E5507281F62C0f8d666beAEA212751cD88994b8",   // Proxy__OVM_L1StandardBridge.json
+    OptimismPortal: "0x3a605B442055DF2898E18cF518feb2e2A6BD0D31",   // OptimismPortalProxy.json
+    L2OutputOracle: "0x5086d1eEF304eb5284A0f6720f79403b4e9bE294",   // L2OutputOracleProxy.json
 }
 
 bridges = {
@@ -59,6 +59,18 @@ crossChainMessenger = new optimismSDK.CrossChainMessenger({
     l2ChainId: await l2Signer.getChainId(),
     l1SignerOrProvider: l1Signer,
     l2SignerOrProvider: l2Signer,
+})
+
+crossChainMessenger2 = new optimismSDK.CrossChainMessenger({
+    bedrock: true,
+    contracts: {
+        l1: l1Contracts
+    },
+    bridges: bridges,
+    l1ChainId: await l1Signer.getChainId(),
+    l2ChainId: await l3Signer.getChainId(),
+    l1SignerOrProvider: l1Signer,
+    l2SignerOrProvider: l3Signer,
 })
 
 
