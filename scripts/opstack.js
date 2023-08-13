d33c6b4c2f84feafa1b241c11b656264e05b50b1
William Wang <bn.william.w@gmail.com>
2023-08-13 09:50:43 -0400

    opstack deploy

--- scripts/opstack.js
+++ scripts/opstack.js
@@ -1,10 +1,22 @@
 optimismSDK = require("@eth-optimism/sdk")
 
+l1Url = "https://optimism-goerli.publicnode.com"
+l2Url = "https://goerli.base.org"
+l3Url = "https://testnet.rpc.zora.energy"
+
 l1Provider = new ethers.providers.JsonRpcProvider(l1Url)
 l2Provider = new ethers.providers.JsonRpcProvider(l2Url)
+l3Provider = new ethers.providers.JsonRpcProvider(l2Url)
+
 l1Signer = new ethers.Wallet(privKey).connect(l1Provider)
 l2Signer = new ethers.Wallet(privKey).connect(l2Provider)
-/*
+
+balances0 = [
+    await l1Provider.getBalance(l1Signer.address),
+    await l2Provider.getBalance(l1Signer.address),
+    await l3Provider.getBalance(l1Signer.address),
+]
+
 zeroAddr = "0x".padEnd(42, "0")
 l1Contracts = {
     StateCommitmentChain: zeroAddr,
@@ -43,22 +55,18 @@ crossChainMessenger = new optimismSDK.CrossChainMessenger({
     l1SignerOrProvider: l1Signer,
     l2SignerOrProvider: l2Signer,
 })
-*/
 
-balances0 = [
-    await l1Provider.getBalance(l1Signer.address),
-    await l2Provider.getBalance(l1Signer.address)
-]
-/*
+
+
 tx = await crossChainMessenger.depositETH(1e9)
 rcpt = await tx.wait()
 
 balances1 = [
     await l1Provider.getBalance(l1Signer.address),
-    await l2Provider.getBalance(l1Signer.address)
+    await l2Provider.getBalance(l1Signer.address),
+    await l3Provider.getBalance(l1Signer.address)
 ]
 
     (balances0[0] - balances1[0]) / 1e9
 
         ((await l2Provider.getBalance(l1Signer.address)) - balances0[1]) / 1e9
-*/
