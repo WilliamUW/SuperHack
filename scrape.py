e9248c02091c37a73054f394e710773cc943454f
William Wang <bn.william.w@gmail.com>
2023-08-12 20:36:34 -0400

    zora

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
  
  
-url = 'https://docs.worldcoin.org/'
+url = 'https://www.mode.network/'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
  
